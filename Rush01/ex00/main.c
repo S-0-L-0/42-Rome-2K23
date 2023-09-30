024a88b349a48d1a99ec69ebfd506f58b1ed5228
Edoardo Forte <edforte@e3r1p8.42roma.it>
2023-09-30 11:55:27 +0200

    ciao

--- Rush01/ex00/main.c
+++ Rush01/ex00/main.c
@@ -1,11 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_memory.c                                  :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edforte <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/28 12:13:08 by edforte           #+#    #+#             */
-/*   Updated: 2023/09/28 12:13:12 by edforte          ###   ########.fr       */
+/*   Created: 2023/09/30 11:19:04 by edforte           #+#    #+#             */
+/*   Updated: 2023/09/30 11:54:37 by edforte          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <unistd.h>
+
+
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	if (argc != 2)
+	{
+		write(2, "Input not valid, try again", 26);
+		return(0);
+	}
+	while (argv[1] != '\0')
+	{
+		if ((argv[1][i] < '1'|| argv[1][i] > '4') || argv[1][i] == ' ')
+		{
+			write(2, "Input not valid, try again", 26);
+			return(0);
+		}
+		i ++;
+	}
+}
+
